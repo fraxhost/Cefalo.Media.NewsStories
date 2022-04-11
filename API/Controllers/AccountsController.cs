8f7d0a39cb5f9531957dd3ffe060c0966f517b4d
Ahmed Ryan <bsse1011@iit.du.ac.bd>
2022-04-11 15:47:29 +0600

    user registration and login successful

---
+++ API/Controllers/AccountsController.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using DB;
+using DB.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Service.DTOs;
+using Service.Interfaces;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ITokenService _tokenService;
+
+        public AccountsController(ApplicationDbContext dbContext, ITokenService tokenService)
+        {
+            _dbContext = dbContext;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.UserId))
+            {
+                return BadRequest("UserId is taken!");
+            }
+
+            using var hmac = new HMACSHA512();
+
+            var user = new User()
+            {
+                UserId = registerDto.UserId,
+                FullName = registerDto.FullName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+
+            return new UserDto
+            {
+                UserId = user.UserId,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _dbContext.Users.SingleOrDefaultAsync(user => user.UserId == loginDto.UserId);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username!");
+            }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid password!");
+                }
+            }
+
+            return new UserDto
+            {
+                UserId = user.UserId,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        private async Task<bool> UserExists(string userId)
+        {
+            return await _dbContext.Users.AnyAsync(user => user.UserId == userId.ToLower());
+        }
+    }
+}
---
+++ DB/Models/Story.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DB.Models
+{
+    public class Story
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime PublishedDate { get; set; }
+
+        
+        public int AuthorId { get; set; }
+        public User Author { get; set; }
+    }
+}
