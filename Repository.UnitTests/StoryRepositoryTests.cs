ed910270fd0d9a04168e234a251995b6585566e1
Ahmed Ryan <bsse1011@iit.du.ac.bd>
2022-05-09 13:35:41 +0600

    first unit test of repository layer

---
+++ Repository.UnitTests/StoryRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using DB;
+using DB.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Repository.UnitTests
+{
+    public class StoryRepositoryTests
+    {
+        public readonly DbContextOptions<ApplicationDbContext> options;
+
+        public StoryRepositoryTests()
+        {
+            options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "NewsStoriesDbTest")
+                .Options;
+        }
+        
+        [Fact]
+        public async Task GetTotalStories_WhenTotalStoriesIsQueried_ReturnsTotalStories()
+        {
+            // Arrange
+            var storiesContext = new ApplicationDbContext(options);
+            var storiesRepository = new StoryRepository(storiesContext);
+
+            var newStory = new Story()
+            {
+                Id = 1,
+                Title = "Story 1",
+                Body = "Body of Story 1",
+                PublishedDate = Convert.ToDateTime("2021-06-24T04:45:23.2321927Z"),
+            };
+            
+            await storiesRepository.CreateStory(newStory);
+            
+            // Act
+            var totalStories = await storiesRepository.GetTotalStories();
+
+            // Assert
+            Assert.Equal(1, totalStories);
+        }
+    }
+}
